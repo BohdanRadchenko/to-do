be492c740159b2b981c5a12e34bf5c0f852f11d7
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-18 10:28:48 +0300

    Updates

---
+++ precache-manifest.99702ec1b57c0c554bd11b6d47b78855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d22498281b6b4a01b63331bb69ae18d",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "e007635b9412e5d3f075",
+    "url": "/to-do/static/css/main.253b9fcf.chunk.css"
+  },
+  {
+    "revision": "563d434ba91893dea5e8",
+    "url": "/to-do/static/js/2.ee07d5b7.chunk.js"
+  },
+  {
+    "revision": "e007635b9412e5d3f075",
+    "url": "/to-do/static/js/main.4235cc38.chunk.js"
+  },
+  {
+    "revision": "7c4529cb5998ccf5e9e5",
+    "url": "/to-do/static/js/runtime~main.c7e19ee4.js"
+  }
+]);
